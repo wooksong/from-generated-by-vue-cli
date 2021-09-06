b2d2ab15f749e13e9d9d9a6e72dc8bbf1b70c2aa
Wook Song <wook16.song@samsung.com>
2021-09-06 13:14:19 +0900

    CI: Add another pipeline to build and deploy on push
    This patch adds a on-push pipeline that builds source code and deploy it
    to GitHub Pages.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  publicPath: process.env.GITHUB_ACTIONS ?
+      '/' + process.env.GITHUB_REPOSITORY_NAME_PART + '/' :
+      '/',
+  outputDir: 'dist',
   assetsDir: 'assets',
   productionSourceMap: false
 }
